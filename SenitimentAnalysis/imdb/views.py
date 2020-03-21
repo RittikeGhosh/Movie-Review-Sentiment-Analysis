38bff24ff3f5ed36d28dbc473bcb27e7549d6f32
rittikeghosh <rittike.11810568@lpu.in>
2020-03-21 16:02:30 +0530

    overview of both pages done partially

--- SenitimentAnalysis/imdb/views.py
+++ SenitimentAnalysis/imdb/views.py
@@ -5,6 +5,13 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('imdb/index.html')
-    return HttpResponse(template.render({}, request))
+    # template = loader.get_template('imdb/index.html')
+    # return HttpResponse(template.render({}, request))
     # return HttpResponse("Welcome")
+    return render(request, 'imdb/index.html')
+
+def details(request):
+    content = {
+        'title' : 'GOT: game of thrones'
+    }
+    return render(request, 'imdb/details.html', content)
